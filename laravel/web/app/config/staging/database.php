4944fbc90747d936d77a82cd89aa50311dc118c4
Paul Schneider <paul.schneider@yepyep.co.uk>
2014-06-26 14:02:30 +0100

    Bug fix for missing comma

--- laravel/web/app/config/staging/database.php
+++ laravel/web/app/config/staging/database.php
@@ -3,6 +3,6 @@
 return [
 	'host' => "localhost",
 	'username'  => 'wfbuildc_b247-com',
-	'password' => 'r]upoyJmL)2t'
-	'database'  => 'b247-com',
+	'password' => 'r]upoyJmL)2t',
+	'database'  => 'b247-com'
 ];
