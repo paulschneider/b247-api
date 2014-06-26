ad1c90e16e04b79e8f379d3270f3bcb77b7ac0c2
Paul Schneider <paul.schneider@yepyep.co.uk>
2014-06-26 15:42:17 +0100

    Env database settings

--- laravel/web/app/config/database.php
+++ laravel/web/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => $_ENV['DATABASE_NAME'],
-			'username'  => $_ENV['DATABASE_USER'],
-			'password'  => $_ENV['DATABASE_PASSWORD'],
+			'database'  => '',
+			'username'  => '',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
---
+++ laravel/web/app/config/staging/database.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'connections' => [
+		'mysql' => [
+			'database' => 'wfstage_b247_com',
+			'username' => 'root',
+			'password' => 'r]upoyJmL)2t'
+		]
+	]
+];
