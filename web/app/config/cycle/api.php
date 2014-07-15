fbb5de6daff3d918e88fe155e325cdcd7fbf624e
Paul Schneider <paul.schneider@yepyep.co.uk>
2014-07-15 09:44:31 +0100

    Cycle server connection details

---
+++ web/app/config/cycle/api.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+	"baseUrl" => "http://app.wf-cycle.co.uk"
+];
---
+++ web/app/config/cycle/database.php
@@ -0,0 +1,13 @@
+<?php
+
+// cycle server database details
+
+return [
+	'connections' => [
+		'mysql' => [
+			'database' => 'wfcycle_b247_com',
+			'username' => 'root',
+			'password' => 'r]upoyJmL)2t'
+		]
+	]
+];
