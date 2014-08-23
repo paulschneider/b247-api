d52e45143732462582d742c78294bf12f455af72
Paul Schneider <paul.schneider@yepyep.co.uk>
2014-08-23 08:18:00 +0100

    Bug fix - syntax error

--- web/app/Apiv1/Responders/ChannelDirectoryResponder.php
+++ web/app/Apiv1/Responders/ChannelDirectoryResponder.php
@@ -1,6 +1,7 @@
 <?php namespace Apiv1\Responders;
 
 use App;
+use Config;
 use Apiv1\Repositories\Channels\Toolbox;
 
 Class ChannelDirectoryResponder {
@@ -18,7 +19,7 @@ Class ChannelDirectoryResponder {
 		$sortedCategories = Toolbox::getCategoryArticleCategories( $categories );
 
 		# create the desired pattern
-		$response = App::make( 'PatternMaker' )->setPattern( 1 )->make( [ 'articles'=> $articles, 'sponsors' => $sponsorResponder->setSponsorType(Config::get('global.sponsorMPU')->getUnassignedSponsors() ] );
+		$response = App::make( 'PatternMaker' )->setPattern( 1 )->make( [ 'articles'=> $articles, 'sponsors' => $sponsorResponder->setSponsorType(Config::get('global.sponsorMPU'))->getUnassignedSponsors() ] );
 
 		# return all the data requested
 		return [
