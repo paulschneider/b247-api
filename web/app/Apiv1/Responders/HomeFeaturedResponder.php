e28c8d55f45b0ecc4fe1392ffc41e538539564ec
Paul Schneider <paul.schneider@yepyep.co.uk>
2014-09-26 11:28:29 +0100

    Limit update for apps

--- web/app/Apiv1/Responders/HomeFeaturedResponder.php
+++ web/app/Apiv1/Responders/HomeFeaturedResponder.php
@@ -6,7 +6,16 @@ class HomeFeaturedResponder {
 
 	public function get()
 	{
-        $articles = App::make('ArticleRepository')->getArticles( 'featured', 15, null, false, true );
+		# we don't want to show as many for the apps
+		if(isMobile()) {
+			$limit = 5;
+		}
+		# as we want to show a lot more
+		else {
+			$limit = 15;
+		}
+
+        $articles = App::make('ArticleRepository')->getArticles( 'featured', $limit, null, false, true );
 
         return App::make('ArticleTransformer')->transformCollection( $articles );
 	}      
